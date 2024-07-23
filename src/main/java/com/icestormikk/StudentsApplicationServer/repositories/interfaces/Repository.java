9969bd6fb1ede9956cc6876c1ff6d0e963b1c630
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-24 01:43:05 +0300

    Обновления в сигнатуре методов основного интерфейса репозитория

--- src/main/java/com/icestormikk/StudentsApplicationServer/repositories/interfaces/Repository.java
+++ src/main/java/com/icestormikk/StudentsApplicationServer/repositories/interfaces/Repository.java
@@ -1,12 +1,14 @@
 package com.icestormikk.StudentsApplicationServer.repositories.interfaces;
 
+import com.icestormikk.StudentsApplicationServer.domain.exceptions.StudentNotFoundException;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface Repository<T, ID> {
     List<T> getAll();
     Optional<T> getById(ID id);
-    T add();
-    T updateById(ID id);
-    void deleteById(ID id);
+    T add(T object);
+    T updateById(ID id, T object) throws StudentNotFoundException;
+    void deleteById(ID id) throws StudentNotFoundException;
 }

ade23831c3156a0f10a0ee47d29596d109c97595
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-23 23:51:07 +0300

    Написал класс для сервиса для работы с объектами класса Student

---
+++ src/main/java/com/icestormikk/StudentsApplicationServer/services/interfaces/StudentService.java
@@ -0,0 +1,5 @@
+package com.icestormikk.StudentsApplicationServer.services.interfaces;
+
+import com.icestormikk.StudentsApplicationServer.domain.Student;
+
+public interface StudentService extends Service<Student, Long> {}

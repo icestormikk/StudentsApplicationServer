038200979ff9e5c4182ad4074a9c3b23bb5a7bc0
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-23 23:51:47 +0300

    Написал конфиг для JDBC-соединения

---
+++ src/main/java/com/icestormikk/StudentsApplicationServer/SpringJdbcConfig.java
@@ -0,0 +1,22 @@
+package com.icestormikk.StudentsApplicationServer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringJdbcConfig {
+    @Bean
+    public DataSource myPostgresDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:15432/students_db");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("root");
+
+        return dataSource;
+    }
+}

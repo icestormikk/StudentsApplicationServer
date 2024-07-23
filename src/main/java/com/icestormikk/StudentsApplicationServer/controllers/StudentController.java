7aff144265b680a0cb1011a4ac2e45fa5d739782
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-24 01:44:09 +0300

    Дописал реализацию недостающих CRUD-операций

--- src/main/java/com/icestormikk/StudentsApplicationServer/controllers/StudentController.java
+++ src/main/java/com/icestormikk/StudentsApplicationServer/controllers/StudentController.java
@@ -1,26 +1,59 @@
 package com.icestormikk.StudentsApplicationServer.controllers;
 
 import com.icestormikk.StudentsApplicationServer.domain.Student;
-import com.icestormikk.StudentsApplicationServer.services.interfaces.StudentService;
+import com.icestormikk.StudentsApplicationServer.services.interfaces.implementations.StudentServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/students")
+@Slf4j
 public class StudentController {
     @Autowired
-    private StudentService studentService;
+    private StudentServiceImpl studentService;
 
     @GetMapping()
     public ResponseEntity<List<Student>> getAllStudents() {
-        List<Student> students = studentService.getALl();
+        List<Student> students = this.studentService.getALl();
         return ResponseEntity.ok(students);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable(name = "id") Long id) {
+        Optional<Student> optionalStudent = this.studentService.getObjectById(id);
+        return optionalStudent.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public ResponseEntity<Student> addStudent(@RequestBody Student studentObj) {
+        try {
+            Student student = this.studentService.addObject(studentObj);
+            return ResponseEntity.ok(student);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable(name = "id") Long id, @RequestBody Student studentObj) {
+        Optional<Student> student = this.studentService.updateObjectById(id, studentObj);
+        return student.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable(name = "id") Long id) {
+        try {
+            this.studentService.deleteObjectBy(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }

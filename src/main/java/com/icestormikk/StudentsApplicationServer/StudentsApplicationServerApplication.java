f8553aa10152719701d3778c709dfa8f8f7e7614
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-23 23:52:58 +0300

    Initial commit for main class

---
+++ src/main/java/com/icestormikk/StudentsApplicationServer/StudentsApplicationServerApplication.java
@@ -0,0 +1,11 @@
+package com.icestormikk.StudentsApplicationServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsApplicationServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsApplicationServerApplication.class, args);
+	}
+}

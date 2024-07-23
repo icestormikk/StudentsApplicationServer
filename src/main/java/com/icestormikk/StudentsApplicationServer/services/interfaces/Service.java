de5916bb900fd091387cbdfbf4b1c44e55b9860d
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-24 01:43:20 +0300

    Обновления в сигнатуре методов основного интерфейса сервисов

--- src/main/java/com/icestormikk/StudentsApplicationServer/services/interfaces/Service.java
+++ src/main/java/com/icestormikk/StudentsApplicationServer/services/interfaces/Service.java
@@ -1,11 +1,14 @@
 package com.icestormikk.StudentsApplicationServer.services.interfaces;
 
+import com.icestormikk.StudentsApplicationServer.domain.Student;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface Service<T, ID> {
     List<T> getALl();
-    T getObjectById(ID id);
+    Optional<T> getObjectById(ID id);
     T addObject(T object);
-    T updateObjectById(ID id,T object);
+    Optional<T> updateObjectById(ID id, T object);
     void deleteObjectBy(ID id);
 }

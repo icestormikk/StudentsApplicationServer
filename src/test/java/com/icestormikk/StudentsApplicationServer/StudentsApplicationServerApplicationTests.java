fdd555b544343136b98f42a47e6bc28f07578e4d
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-08-02 17:16:05 +0300

    Обновил номер версии приложения

---
+++ src/test/java/com/icestormikk/StudentsApplicationServer/StudentsApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icestormikk.StudentsApplicationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
